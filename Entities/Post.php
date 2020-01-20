0ca75091aa39d663b2e8ba5fb9e8171f0f222555
Jndantas <jailton.dantass@gmail.com>
2020-01-20 17:42:51 -0300

    first commit

---
+++ Entities/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Blog\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['title','body'];
+
+    protected function getAuthorAttribute()
+    {
+        return 'Jailton Dantas';
+    }
+}
